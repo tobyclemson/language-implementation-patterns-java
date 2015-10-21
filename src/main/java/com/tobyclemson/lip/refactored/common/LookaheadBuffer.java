4ccb10ae4618f397d4b209272f985b79f046551a
Toby Clemson <tobyclemson@gmail.com>
2015-10-21 13:40:35 +0100

    Use lombok in LookaheadBuffer.

--- src/main/java/com/tobyclemson/lip/refactored/common/LookaheadBuffer.java
+++ src/main/java/com/tobyclemson/lip/refactored/common/LookaheadBuffer.java
@@ -1,16 +1,18 @@
 package com.tobyclemson.lip.refactored.common;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class LookaheadBuffer<T> {
-    private final Reader<T> reader;
-    private T lookahead;
+    Reader<T> reader;
+    @Getter @NonFinal T lookahead;
 
     public LookaheadBuffer(Reader<T> reader) {
         this.reader = reader;
         lookahead = reader.readNext();
-    }
-
-    public T getLookahead() {
-        return lookahead;
     }
 
     public void advance() {
